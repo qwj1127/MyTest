32ec11dfa64f59f851ec6ce36156a16e1c09f4cd
qinwenjun <qwj1127@163.com>
2018-11-21 16:00:21 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router' // 引用并使用路由配置文件中的内容
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>' // 如果vue的根实例中有模板存在，则模板会替换挂载的元素
 })
