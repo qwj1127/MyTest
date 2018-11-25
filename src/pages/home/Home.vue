dd8cb989c833dd7d7da92054527ef5d8eab9f620
qinwenjun <qwj1127@163.com>
2018-11-25 16:24:05 +0800

    add icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header> <!--使用子组件-->
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header' // 引入子组件，勿忘！！！
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default{
   name: 'Home',
   components: {
-    HomeHeader,
-    HomeSwiper
+    HomeHeader, // 注册子组件
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -5,7 +5,7 @@
           v-for="item of list"
           :key="item.id">
           <img class="swiper-img" :src="item.imgUrl"/>
-    </swiper-slide>
+      </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
   </div>
@@ -19,7 +19,8 @@ export default{
       list: [{'id': '0001', 'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1806/5f/171e04d556e40a02.jpg_750x200_424c4ddd.jpg'}, {'id': '0002', 'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1807/a1/41a802abfc4f0202.jpg_750x200_9f0cf69c.jpg'}],
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop: true
+        loop: true,
+        autoplay: 3000
       }
     }
   }
@@ -31,6 +32,7 @@ export default{
     background: red !important
   .wrapper
     overflow: hidden
+    width: 100%
     height: 0
     padding-bottom: 26.67%
     .swiper-img
